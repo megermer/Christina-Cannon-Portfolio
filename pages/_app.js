4fea23c9dc50dd89f33625f121da189d28a229c8
megermer <113950667+megermer@users.noreply.github.com>
2023-06-17 15:11:26 -0400

    Commit before upgrading the link

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "../styles/globals.css";
+import { Libre_Baskerville } from "@next/font/google";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const baskerville = Libre_Baskerville({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
+function App({ Component, pageProps }) {
+  return (
+    <main className={baskerville.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
 export default App;
---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import Link from "next/link";
+import { useTina } from "tinacms/dist/react";
+import { client } from "../tina/__generated__/client";
+import Image from "next/image";
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+
+export default function Home(props) {
+  // data passes though in production mode and data is updated to the sidebar data in edit-mode
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+
+  return (
+    <main className="h-screen w-screen">
+      <div className="absolute -z-10 h-full w-full bg-black">
+        <Image
+          src="/uploads/bridgePhoto.jpg"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-50"
+        />
+      </div>
+      {(data?.page?.rows || []).map((row, i) => (
+        <div key={i} className="text-white text-center">
+          <TinaMarkdown content={row.block} />
+        </div>
+      ))}
+    </main>
+  );
+}
+
+export const getStaticProps = async () => {
+  const { data, query, variables } = await client.queries.page({
+    relativePath: "home.json",
+  });
+
+  return {
+    props: {
+      data,
+      query,
+      variables,
+      //myOtherProp: 'some-other-data',
+    },
+  };
+};

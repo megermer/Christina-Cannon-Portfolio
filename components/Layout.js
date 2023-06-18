2109ddfba96f5b49592c881f22361b03f011441f
megermer <113950667+megermer@users.noreply.github.com>
2023-06-17 20:53:45 -0400

    Add signatureFern image to nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 
 export const Layout = (props) => {
   return (
@@ -13,10 +14,17 @@ export const Layout = (props) => {
         <meta name="description" content="A TinaCMS Application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <Link href="/">Home</Link>
+      <header className=" flex flex-c -screen w-screen justify-center">
+        <Image
+          src="/uploads/signatureFerns.png"
+          width={300}
+          height={100}
+          alt="red ferns"
+        />
+
+        {/* <Link href="/">Home</Link>
         {" | "}
-        <Link href="/posts">Posts</Link>
+        <Link href="/posts">Posts</Link> */}
       </header>
       <main>{props.children}</main>
     </div>

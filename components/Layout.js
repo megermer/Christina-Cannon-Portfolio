880ca040bb23fbf0f111319259fd0ca5c9daf033
megermer <113950667+megermer@users.noreply.github.com>
2023-06-23 20:59:45 -0400

    Center links and removes top padding for navbar

--- components/Layout.js
+++ components/Layout.js
@@ -4,24 +4,27 @@ import Image from "next/image";
 
 export const Layout = (props) => {
   return (
-    <div
-      style={{
-        margin: "3rem",
-      }}
-    >
+    <div>
       <Head>
         <title>Tina App</title>
         <meta name="description" content="A TinaCMS Application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className=" flex flex-c -screen w-screen justify-center">
+      <header className="flex flex-col w-screen items-center mt-5">
         <Image
           src="/uploads/signatureFerns.png"
           width={300}
           height={100}
           alt="red ferns"
         />
-
+        <div className="w-screen flex flex-row gap-20 justify-center mt-3">
+          <Link href="">about</Link>
+          <Link href="">art</Link>
+          <Link href="">writing</Link>
+          <Link href="">research</Link>
+          <Link href="">blog</Link>
+          <Link href="">contact</Link>
+        </div>
         {/* <Link href="/">Home</Link>
         {" | "}
         <Link href="/posts">Posts</Link> */}
